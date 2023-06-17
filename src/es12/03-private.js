aff691a80e8bb10b89dae722b57b55d44fee8954
Javier Eduardo Quiroga Amaya <jeqa2004.7916@gmail.com>
2023-06-16 21:07:29 -0400

    Agrego los componentes de ECMAScript 12.

---
+++ src/es12/03-private.js
@@ -0,0 +1,29 @@
+class User {
+    constructor(name, age) {
+        console.log('New User');
+        this.name = name;
+        this.age = age;
+    }
+
+    #speak() {
+        return 'Hello';
+    }
+
+    greeting() {
+        return `${this.speak()}, I am ${this.name}`;
+    }
+
+    get #uAge() {
+        return this.age;
+    }
+
+    set #uAge(n) {
+        this.age = n;
+    }
+}
+
+const newUser = new User('Javier', 18);
+
+console.log(newUser.greeting());
+console.log(newUser.uAge);
+console.log(newUser.uAge = 20);
